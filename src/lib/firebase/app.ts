54808b9335939cb785cf73a79de75d55b71c37a6
L33t-Rose <jphillipjean@gmail.com>
2023-09-30 13:57:49 -0400

    Added Cloudflare Adapter

--- src/lib/firebase/app.ts
+++ src/lib/firebase/app.ts
@@ -1,4 +1,4 @@
-import { initializeApp, getApp, getApps } from 'firebase/app';
+import { initializeApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import {
 	PUBLIC_API_KEY,
--- src/lib/server/admin.ts
+++ src/lib/server/admin.ts
@@ -1,6 +1,4 @@
 import { FIREBASE_SERVER_CONFIG } from '$env/static/private';
 import { FlarebaseAuth } from '@marplex/flarebase-auth';
 
-console.log('server', JSON.parse(FIREBASE_SERVER_CONFIG));
 export const app = new FlarebaseAuth(JSON.parse(FIREBASE_SERVER_CONFIG));
-console.log(app);
